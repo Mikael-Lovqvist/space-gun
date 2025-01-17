5993881412b3ee2f63c71f129d4e896284af52c3
Mikael Lövqvist <mikael.lovqvist.mail@gmail.com>
2025-01-17 21:41:05 +0100

    Implemented more formatting and parsing

--- test/object.mjs
+++ test/object.mjs
@@ -2,8 +2,11 @@ import { Celestial_Orbit } from '../lib/celestial_orbit.mjs';
 
 
 const o = new Celestial_Orbit({
-	position: 0,
-	semi_major_axis: 149.598e9, //m
+	position: [-100, '1 AU', 50000000],
+	semi_major_axis: '1 AU',
+
 });
 
-console.log(o.format_field_value('semi_major_axis'));
+console.log(o);
+
+console.log(o.format_field_value('position'));
--- test/vector.mjs
+++ test/vector.mjs
@@ -1,4 +1,5 @@
-import { SI_Prefix, Scalar, Vector_3 } from '../lib/vectors.mjs';
+import { Scalar, Vector_3 } from '../lib/vectors.mjs';
+import { SI_Prefix } from '../lib/prefix.mjs';
 
 
 console.log(Vector_3.create_from_anything(1.23));
@@ -9,3 +10,6 @@ console.log(Vector_3.create_from_anything([1, 2, 3]));
 
 console.log(Scalar.format_value_with_unit(123, '°'));
 console.log(Scalar.format_value_with_unit(2384, 'm', SI_Prefix));
+
+const V = Vector_3.create_from_anything(0.000000001, .00000000002, 3e9);
+console.log(Vector_3.format_value_with_unit(V, 'm', SI_Prefix));
