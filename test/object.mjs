c265daed3b48ef1fa93492c9757ee4b534b69b68
Mikael Lövqvist <mikael.lovqvist.mail@gmail.com>
2025-01-17 22:18:59 +0100

    Implemented all fields of Celestial_Orbit (unless we have been fooled by chatgpt)

--- test/object.mjs
+++ test/object.mjs
@@ -2,11 +2,20 @@ import { Celestial_Orbit } from '../lib/celestial_orbit.mjs';
 
 
 const o = new Celestial_Orbit({
-	position: [-100, '1 AU', 50000000],
-	semi_major_axis: '1 AU',
+	position: 0,
+	semi_major_axis: '149598023 km',
+	eccentricity: 0.0167086,
+	inclination: [7.155, 1.57869, 0.00005],
+	longitude_asc_node: -11.26064,
+	arg_of_periapsis: 114.20783,
+	mean_anomaly: 358.617,
+	epoch: 'J2000',
+	orbital_period: '365.256363004 d',
 
 });
 
 console.log(o);
 
 console.log(o.format_field_value('position'));
+console.log(o.format_field_value('inclination'));
+console.log(o.format_field_value('orbital_period'));
